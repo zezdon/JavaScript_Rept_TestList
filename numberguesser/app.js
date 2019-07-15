6df8bb7000bf12910690cc7692781170fdc63835
Mats G <ed54res@hotmail.com>
2019-07-15 21:13:45 +0200

    Play Again and Guess Random Number

--- numberguesser/app.js
+++ numberguesser/app.js
@@ -10,7 +10,7 @@ GAME FUNCTION:
 // Game values
 let min = 1,
   max = 10,
-  winningNum = 2,
+  winningNum = getRandomNum(min, max),
   guessesLeft = 3;
 
 // UI Elements
@@ -24,6 +24,13 @@ const game = document.querySelector('#game'),
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// Play again event listener
+game.addEventListener('mousedown', function(e){
+  if(e.target.className === 'play-again'){
+    window.location.reload();
+  }
+});
 
 // Listen for guess
 guessBtn.addEventListener('click', function () {
@@ -91,7 +98,17 @@ function gameOver(won, msg) {
       message.style.color = color;
       // Set message
       setMessage(msg, color);
+
+      //Play Again?
+      guessBtn.value = 'Play Again';
+      guessBtn.className += 'play-again';
 }
+
+// Get Winning Number
+function getRandomNum(min, max){
+  //console.log(Math.floor(Math.random()*(max-min+1)+min));
+  return Math.floor(Math.random()*(max-min+1)+min);  
+} 
 
 // Set message
 function setMessage(msg, color) {
