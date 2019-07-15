cd783e9160d5d41e26cac88bfe18c0c75ed47d9b
uberb1 <uberb1@unsafe.mats6.com>
2019-07-15 12:03:29 +0200

    Remove tasks

--- tasklist/app.js
+++ tasklist/app.js
@@ -12,6 +12,8 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
 }
 
 // Add Task
@@ -42,4 +44,13 @@ function addTask(e) {
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    //if(confirm('Are You Sure?')) {
+      e.target.parentElement.parentElement.remove();
+    //}
+  }
 }
