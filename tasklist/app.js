6b7ff19fd94322d1a2299433b5183a612a23842b
uberb1 <uberb1@unsafe.mats6.com>
2019-07-15 13:03:42 +0200

    Read and Write to LocalStorage

--- tasklist/app.js
+++ tasklist/app.js
@@ -10,6 +10,8 @@ loadEventListeners();
 
 // Load all event listeners
 function loadEventListeners() {
+  // DOM Load event
+  document.addEventListener('DOMContentLoaded', getTasks);  
   // Add task event
   form.addEventListener('submit', addTask);
   // Remove task event
@@ -18,6 +20,36 @@ function loadEventListeners() {
   clearBtn.addEventListener('click', clearTasks); 
   // Filter tasks event
   filter.addEventListener('keyup', filterTasks);   
+}
+
+// Get Tasks from LocalStorage
+function getTasks() {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function (task) {
+    // Create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(task));
+    // Create new link element
+    const link = document.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append the link to li
+    li.appendChild(link);
+
+    // Append li to ul
+    taskList.appendChild(li);
+  });
 }
 
 // Add Task
@@ -44,10 +76,28 @@ function addTask(e) {
   // Append li to ul
   taskList.appendChild(li);
 
+  // Store in LocalStorage
+  storeTaskInLocalStorage(taskInput.value);  
+
   // Clear input
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+// Store Task
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  //check if nothing in there
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  //push variable task(which is the value)
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));   
 }
 
 // Remove Task
